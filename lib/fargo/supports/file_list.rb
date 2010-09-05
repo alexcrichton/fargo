3dc644c35994ba1c72eae35b032b0cf095768f28
Alex Crichton <alex@alexcrichton.com>
2010-09-05 00:50:14 -0400

    Don't store the parsed file list, just the file where it's located

--- lib/fargo/supports/file_list.rb
+++ lib/fargo/supports/file_list.rb
@@ -4,20 +4,19 @@ require 'libxml'
 module Fargo
   module Supports
     module FileList
-      
       class Listing < Struct.new(:tth, :size, :name); end
 
       # Lazily load the file list for the nick. Subscribe to the client for the
       # event :file_list to get notified.
       def file_list nick
         @file_list ||= {}
-        return @file_list[nick] if @file_list.has_key?(nick)
+        return parse_file_list(@file_list[nick]) if @file_list.has_key?(nick)
 
         file_gotten = lambda{ |type, map| 
           case type
             when :download_finished, :download_failed, :connection_timeout
               if map[:nick] == nick
-                @file_list[nick] = parse_file_list map[:file]
+                @file_list[nick] = map[:file]
                 unsubscribe &file_gotten
                 publish :file_list, :nick => nick, :list => @file_list[nick]
               end
@@ -28,16 +27,16 @@ module Fargo
 
         download nick, 'files.xml.bz2'
       end
-      
+
       # Wait for the results, don't get them just yet
       def file_list! nick
         @file_list ||= {}
-        return @file_list[nick] if @file_list.has_key?(nick)
+        return parse_file_list(@file_list[nick]) if @file_list.has_key?(nick)
 
-        file_list = nil
+        list = nil
         list_gotten = lambda{ |type, map| 
           if type == :file_list && map[:nick] == nick
-            file_list = map[:list]
+            list = map[:list]
             true
           else
             false
@@ -46,11 +45,11 @@ module Fargo
 
         timeout_response(10, list_gotten){ file_list nick }
 
-        file_list
+        parse_file_list list
       end
 
       private
-      
+
       def parse_file_list file
         if file && File.exists?(file)
           xml = Bzip2::Reader.open(file).read
@@ -61,7 +60,7 @@ module Fargo
           nil
         end
       end
-      
+
       def construct_file_list node
         list = {}
 
