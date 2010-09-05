fc7cafa96d86bc400426deefdc9414f7484c7694
Alex Crichton <alex@alexcrichton.com>
2010-09-04 21:02:02 -0400

    Add a lazy load and a waiting for load method of the file lists

--- lib/fargo/supports/file_list.rb
+++ lib/fargo/supports/file_list.rb
@@ -7,39 +7,60 @@ module Fargo
       
       class Listing < Struct.new(:tth, :size, :name); end
 
+      # Lazily load the file list for the nick. Subscribe to the client for the
+      # event :file_list to get notified.
       def file_list nick
         @file_list ||= {}
+        return @file_list[nick] if @file_list.has_key?(nick)
 
-        @file_list[nick] ||= begin
-          downloaded_file = nil
-
-          file_gotten = lambda{ |type, map| 
-            case type
-              when :download_finished, :download_failed, :connection_timeout
-                # TODO: better way to do this?
-                if map[:nick] == nick
-                  downloaded_file = map[:file]
-                end
-                map[:nick] == nick
-              else
-                false
-            end
-          }
-
-          timeout_response(10, file_gotten){ download nick, 'files.xml.bz2' }
-
-          if downloaded_file && File.exists?(downloaded_file)
-            xml = Bzip2::Reader.open(downloaded_file).read
-            doc = LibXML::XML::Document.string xml
-
-            construct_file_list doc.root
-          else
-            nil
+        file_gotten = lambda{ |type, map| 
+          case type
+            when :download_finished, :download_failed, :connection_timeout
+              if map[:nick] == nick
+                @file_list[nick] = parse_file_list map[:file]
+                unsubscribe &file_gotten
+                publish :file_list, :nick => nick, :list => @file_list[nick]
+              end
           end
-        end
+        }
+
+        subscribe &file_gotten
+
+        download nick, 'files.xml.bz2'
+      end
+      
+      # Wait for the results, don't get them just yet
+      def file_list! nick
+        @file_list ||= {}
+        return @file_list[nick] if @file_list.has_key?(nick)
+
+        file_list = nil
+        list_gotten = lambda{ |type, map| 
+          if type == :file_list && map[:nick] == nick
+            file_list = map[:list]
+            true
+          else
+            false
+          end
+        }
+
+        timeout_response(10, list_gotten){ file_list nick }
+
+        file_list
       end
 
       private
+      
+      def parse_file_list file
+        if file && File.exists?(file)
+          xml = Bzip2::Reader.open(file).read
+          doc = LibXML::XML::Document.string xml
+
+          construct_file_list doc.root
+        else
+          nil
+        end
+      end
       
       def construct_file_list node
         list = {}
