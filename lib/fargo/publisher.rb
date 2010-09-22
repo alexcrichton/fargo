62d25de5cec513e344017cfa87583d6363d0f3ba
Alex Crichton <alex@alexcrichton.com>
2010-09-22 00:18:40 -0400

    Ignore subscriptions which produce errors but log errors about them.

--- lib/fargo/publisher.rb
+++ lib/fargo/publisher.rb
@@ -1,26 +1,42 @@
 module Fargo
   module Publisher
-    
+
     attr_reader :subscribers
 
     def subscribe &subscriber
-      raise RuntimeError.new("Need a subscription block!") if subscriber.nil?
+      raise RuntimeError.new('Need a subscription block!') if subscriber.nil?
+
       Fargo.logger.debug "#{self}: subscribing #{subscriber}"
       (@subscribers ||= []) << subscriber
     end
-    
+
     def subscribed_to?
       @subscribers && @subscribers.size > 0
     end
-    
+
     def unsubscribe &subscriber
-      raise RuntimeError.new("Need a subscription block!") if subscriber.nil?
+      raise RuntimeError.new('Need a subscription block!') if subscriber.nil?
+
       Fargo.logger.debug "#{self}: unsubscribing #{subscriber}"
       (@subscribers ||= []).delete subscriber
     end
 
     def publish message_type, hash = {}
-      @subscribers.each { |subscriber| subscriber.call message_type, hash } if @subscribers
+      @subscribers ||= []
+
+      to_remove = []
+
+      @subscribers.each do |subscriber|
+        begin
+          subscriber.call message_type, hash
+        rescue => e
+          Fargo.logger.warn "#{self}: error publishing to: #{subscriber}! #{e}"
+          to_remove << subscriber
+        end
+      end
+
+      @subscribers -= to_remove
     end
+
   end
 end
