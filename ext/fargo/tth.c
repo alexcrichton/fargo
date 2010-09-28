430ec478153301a337036af01d373e5dac4ced4d
Alex Crichton <alex@alexcrichton.com>
2010-09-27 21:07:15 -0400

    Remove unneeded code. Or at least I hope...

--- ext/fargo/tth.c
+++ ext/fargo/tth.c
@@ -26,7 +26,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <assert.h>
 
 #include "tigertree.h"
 #include "base32.h"
@@ -61,7 +60,7 @@ char* tth(const char* filename, char **tthl, size_t *tthl_len)
     TT_CONTEXT tt;
     unsigned char buf[1 + 512 * BLOCKSIZE];
     struct stat sb;
-    unsigned leaf_cnt, level;
+    unsigned level;
     size_t leaf_blocksize;
 
     int fd = open(filename, O_RDONLY);
@@ -72,26 +71,8 @@ char* tth(const char* filename, char **tthl, size_t *tthl_len)
     level = calc_block_level(sb.st_size, max_block_count);
     leaf_blocksize = 1 << (level+10);
 
-    //TRACE(("level == %d, leaf_blocksize == %d\n", level, leaf_blocksize));
-
-    /* get memry for leaves */
-    leaf_cnt = sb.st_size / leaf_blocksize;
-    if (sb.st_size % leaf_blocksize)
-      leaf_cnt++;
-
-    //TRACE(("leaf_cnt == %d\n", leaf_cnt));
-
-    /*
-    if (leaf_cnt >= 2) {
-        *tthl_len = leaf_cnt * TIGERSIZE;
-        *tthl = malloc(leaf_cnt * TIGERSIZE);
-        if (*tthl == NULL)
-            return NULL;
-    } else */
-    {
-        *tthl_len = 0;
-        *tthl = NULL;
-    }
+    *tthl_len = 0;
+    *tthl = NULL;
 
     tt_init(&tt, *tthl, level);
     tt.leaf = buf;
@@ -112,14 +93,6 @@ char* tth(const char* filename, char **tthl, size_t *tthl_len)
     close(fd);
 
     tt_digest(&tt, root);
-
-    if (*tthl_len) {
-        if (*tthl_len != (tt.tthl - *(unsigned char **)tthl)) {
-            fprintf(stderr, "*tthl_len == %lu, (tt.tthl - *(unsigned char **)tthl) == %ld\n", *tthl_len, (tt.tthl - *(unsigned char **)tthl));
-            fflush(stderr);
-        }
-        assert(*tthl_len == (tt.tthl - *(unsigned char **)tthl));
-    }
 
     tth = base32_encode(root, sizeof(root));
 
