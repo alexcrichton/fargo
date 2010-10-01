6077e5d7639b6274b1510fe2d0ebd7fc475908ae
Alex Crichton <alex@alexcrichton.com>
2010-09-30 20:50:24 -0400

    Add a spec for getting the list of nicks

--- spec/fargo/supports/nick_list_spec.rb
+++ spec/fargo/supports/nick_list_spec.rb
@@ -13,15 +13,23 @@ describe Fargo::Supports::NickList, :type => :em do
   end
 
   it "uses results from search queries for open slot information" do
-    @client.channel.push [:search_result, {:nick => 'nick', :open_slots => 0}]
+    @client.channel << [:search_result, {:nick => 'nick', :open_slots => 0}]
     @client.nick_has_slot?('nick').should be_false
 
-    @client.channel.push [:search_result, {:nick => 'nick', :open_slots => 1}]
+    @client.channel << [:search_result, {:nick => 'nick', :open_slots => 1}]
     @client.nick_has_slot?('nick').should be_true
 
     t = Time.now + 20.minutes
     Time.stub(:now).and_return t
     @client.stub(:info).with('nick').and_return nil
     @client.nick_has_slot?('nick').should be_false
+  end
+
+  it "gets the nick list from the subscribed channel" do
+    @client.nicks.should == []
+
+    @client.channel << [:nick_list, {:nicks => ['a', 'b']}]
+
+    @client.nicks.should == ['a', 'b']
   end
 end
