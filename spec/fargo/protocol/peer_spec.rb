f3608d1d7252bcb958f025b7e0cdc183ddfde829
Alex Crichton <alex@alexcrichton.com>
2010-10-04 17:08:39 -0400

    Initial spec for the initial handshake with a client

--- spec/fargo/protocol/peer_spec.rb
+++ spec/fargo/protocol/peer_spec.rb
@@ -2,4 +2,34 @@ require 'spec_helper'
 
 describe Fargo::Protocol::Peer do
 
+  let(:client) { Fargo::Client.new }
+
+  let(:conn) {
+    helper_object(described_class).tap do |conn|
+      conn.stub :set_comm_inactivity_timeout
+      conn.post_init
+      conn.client = client
+    end
+  }
+
+  include Fargo::Utils
+
+  describe "the client to client handshake" do
+    before :each do
+      client.config.nick = 'fargo'
+    end
+
+    it "responds with all the correct info when the remote nick is received" do
+      conn.should_receive(:send_data).with('$MyNick fargo|').ordered
+      conn.should_receive(:send_data).with(/^\$Lock \w+ Pk=\w+\|$/).ordered
+      conn.should_receive(:send_data).with(/^\$Supports (\w+ ?)+\|$/).ordered
+      conn.should_receive(:send_data).with(
+        /^\$Direction (Download|Upload) \d+\|$/).ordered
+      conn.should_receive(:send_data).with(
+        "$Key #{generate_key('lock')}|").ordered
+
+      conn.receive_data '$MyNick foobar|$Lock lock Pk=pk|'
+    end
+  end
+
 end
