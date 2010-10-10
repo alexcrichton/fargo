b13fa2c49fbf7b8dbbf1b6373edddcb1e5981830
Alex Crichton <alex@alexcrichton.com>
2010-10-10 14:50:55 -0400

    Allow sorting of nicks based on certain values

--- lib/fargo/cli.rb
+++ lib/fargo/cli.rb
@@ -108,9 +108,28 @@ module Fargo
         end
       end
 
-      def who
-        client.nicks.each do |n|
-          printf "%10s %s\n", humanize_bytes(client.info(n)[:sharesize]), n
+      def who sort_by = nil
+        print_nick = lambda{ |n, s| printf "%10s %s\n", humanize_bytes(s), n }
+
+        if sort_by.nil?
+          client.nicks.each do |n|
+            print_nick.call n, client.info(n)[:sharesize]
+          end
+        else
+          pairs = client.nicks.map{ |n|
+            [n, client.info(n)[:sharesize]]
+          }
+
+          if sort_by == 'name'
+            pairs = pairs.sort_by{ |p| p[0] }
+          elsif sort_by == 'size'
+            pairs = pairs.sort_by{ |p| p[1] }
+          else
+            pairs = []
+            puts "Unknown sorting by: #{sort_by.inspect}"
+          end
+
+          pairs.each &print_nick
         end
 
         true
