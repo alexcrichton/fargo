b2adc7045d4cba376d4dcde72944e62826978d19
Alex Crichton <alex@alexcrichton.com>
2010-12-20 16:40:51 -0600

    Adding stricter warnings

--- ext/readline/extconf.rb
+++ ext/readline/extconf.rb
@@ -4,6 +4,8 @@ if RUBY_VERSION =~ /1\.9/
   $CFLAGS << ' -DRUBY_19'
 end
 
+$CFLAGS << ' -pedantic -Wall -Wextra -Wno-unused'
+
 if have_header('readline/readline.h') || have_header('readline.h')
   create_makefile('extra_utils')
 end
