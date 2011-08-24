513a4e4224a7bcb4038d6b7ba90b90f9249afbb3
Alex Crichton <alex@alexcrichton.com>
2011-08-24 14:10:18 -0400

    Re-work the #info and #nick_has_slot? interface to take blocks which accept callbacks

--- lib/fargo/supports/nick_list.rb
+++ lib/fargo/supports/nick_list.rb
@@ -19,48 +19,65 @@ module Fargo
         hub.send_message 'UserIP', nicks.flatten.join('$$')
       end
 
-      def info nick
-        if @nick_info.has_key?(nick) || !connected? || !@nicks.include?(nick)
-          return @nick_info[nick]
-        end
-
-        # If we're connected and we don't already have this user's info, ask the
-        # server. We'll wait for 5 second to respond, otherwise we'll just
-        # return nil and be done with it
-        info_gotten = lambda{ |type, map|
-          type == :myinfo && map[:nick].to_s == nick.to_s
+      # Send a $GetINFO request to the hub for the specified peer.
+      #
+      # @param [String] nick the peer to get information about
+      # @param [Integer] timeout the maximum amount of time (in seconds) to wait
+      #   before the supplied block will be called.
+      # @param [Proc] block once information is retrieved, then this callback
+      #   will be invoked with the info retrieved from the nick. If the timeout
+      #   expires before we have information, the block will be invoked with
+      #   nil.
+      def info nick, timeout = 5, &block
+        deferrable = EventMachine::DefaultDeferrable.new
+        @info_deferrables[nick] = deferrable
+        deferrable.timeout timeout
+        deferrable.callback &block
+        deferrable.errback {
+          @info_deferrable.delete nick
+          block.call nil
         }
-        timeout_response(5, info_gotten){ get_info nick }
 
-        @nick_info[nick]
+        get_info nick
       end
 
-      # This query must be up to date so remove any cached information we have
-      # about the nick so we can get a fresh copy
-      def nick_has_slot? nick
-        if @search_result_slots[nick] &&
-            @search_result_slots[nick][:updated_at] + 10.minutes > Time.now
-          return @search_result_slots[nick][:slots] > 0
+      # Test whether the remote peer has a slot for downloading a file. This
+      # information might be cached, so the supplied block might be invoked
+      # immediately.
+      #
+      # @param [String] nick the remote peer to inspect
+      # @param [Proc] block the callback which will be invoked with one
+      #   boolean parameter whether the nick had a slot or not.
+      def nick_has_slot? nick, &block
+        raise ArgumentError.new 'Need a block!' if block.nil?
+        if @search_result_slots.key?(nick) &&
+           Time.now < @search_result_slots[nick][:updated_at] + 1.minute
+          block.call @search_result_slots[nick][:slots] > 0
         end
 
-        @nick_info.try :delete, nick
-        info = info nick
+        info nick do |map|
+          if map.nil?
+            # Timed out? No slots...
+            block.call false
+          elsif map[:interest].nil?
+            # No interest given, assume has a slot
+            block.call true
+          elsif match = info[:interest].match(/.*?<.*S:(\d+).*>/)
+            debug 'slots', "User: #{nick} has #{match[1]} open slots"
+            block.call match[1].to_i > 0
+          else
+            # Couldn't parse, assume has a slot
+            block.call true
+          end
+        end
 
-        return false if info.nil?
-        return true  if info[:interest].nil?
-
-        match = info[:interest].match /.*?<.*S:(\d+).*>/
-        return true if match.nil?
-
-        debug 'slots', "User: #{nick} has #{match[1]} open slots"
-        match[1].to_i > 0
       end
 
       protected
 
       def initialize_nick_lists
         @nicks     = []
-        @nick_info = Hash.new{ |h, k| h[k] = {} }
+        @info_deferrables = {}
         @search_result_slots = {}
 
         channel.subscribe do |type, map|
@@ -68,15 +85,18 @@ module Fargo
             when :hello
               @nicks << map[:nick] unless @nicks.include? map[:nick]
             when :myinfo
-              @nick_info[map[:nick]] = map
+              deferrable = @info_deferrables.delete map[:nick]
+              deferrable.succeed map if deferrable
             when :nick_list
               @nicks = map[:nicks]
             when :quit
+              deferrable = @info_deferrables.delete map[:nick]
+              deferrable.fail if deferrable
               @nicks.delete map[:nick]
-              @nick_info.delete map[:nick]
             when :hub_disconnected
               @nicks.clear
-              @nick_info.clear
+              @info_deferrables.each_value { |v| v.fail }
+              @info_deferrables.clear
             when :userip
               map[:users].each_pair{ |nick, ip| @nick_info[nick][:ip] = ip }
             when :search_result
