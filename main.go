1236b99f2dc9a6a0a896e8f430c207b7469ac934
Alex Crichton <alex@alexcrichton.com>
2012-07-21 03:30:08 -0400

    Remove unnecessary ip() method from main.go

--- main.go
+++ main.go
@@ -4,26 +4,10 @@ import "bufio"
 import "flag"
 import "io"
 import "log"
-import "net"
 import "os"
 
 import "./ui"
 import "./dc"
-
-func ip() string {
-  name, err := os.Hostname()
-  if err != nil {
-    log.Fatal(err)
-  }
-  addrs, err := net.LookupHost(name)
-  if err != nil {
-    log.Fatal(err)
-  }
-  if len(addrs) == 0 {
-    log.Fatal("no ip address")
-  }
-  return addrs[0]
-}
 
 func main() {
   client := dc.NewClient()
